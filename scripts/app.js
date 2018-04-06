41744b1de1dd36cedbb88ae505432795707f6ec7
amgranad <ambergkim@gmail.com>
2018-04-06 11:18:21 -0700

    UI improvements

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,17 @@
 angular.module("todoListApp", [])
 
 .controller('mainCtrl', function($scope) {
-  $scope.helloWorld = function() {
-    console.log("Hello there! This the helloWorld controller function, in the mainCtrl!");
+  $scope.learningNgChange = function() {
+    console.log('an input changed');
   };
+
+  $scope.todos = [
+    {"name": "clean the house"},
+    {"name": "water the dog"},
+    {"name": "feed the lawn"},
+    {"name": "pay dem bills"},
+    {"name": "run"},
+    {"name": "swim"}
+  ]
+  
 });
